2689ffa957726a0280b62d790245f533ac3e4760
TioStitch <87840489+TioStitch@users.noreply.github.com>
2024-04-19 13:05:42 -0300

    Delete InventoryUtils.lua

--- InventoryUtils.lua
+++
@@ -1,9 +0,0 @@
-local inventoryUtils = {}
-
-function inventoryUtils:printItems(inv)
-    for key, value in ipairs(inv) do
-        print(key .. ". " .. value)
-    end
-end
-
-return inventoryUtils
